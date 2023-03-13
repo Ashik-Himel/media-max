adb40535a7e50d771ed9ce553b70052672d67466
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-03-13 11:42:11 +0600

    javascript prettier formatted

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const bar = document.querySelector(".header-section .fa-bars-staggered");
 const cross = document.querySelector(".header-section .fa-xmark");
 const navMenu = document.querySelector(".header-section nav");
 
-bar.addEventListener("click", function() {
+bar.addEventListener("click", function () {
   navMenu.classList.add("active");
-})
-cross.addEventListener("click", function() {
+});
+cross.addEventListener("click", function () {
   navMenu.classList.remove("active");
-})
+});
