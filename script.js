f87560abf86d00ab82d5826b33deeb61b243b58d
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-03-13 22:41:47 +0600

    team section added with responsive

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const bar = document.querySelector(".header-section .fa-bars-staggered");
 const cross = document.querySelector(".header-section .fa-xmark");
+const navLink = document.querySelectorAll(".header-section nav a");
 const navMenu = document.querySelector(".header-section nav");
 
 bar.addEventListener("click", function () {
@@ -8,3 +9,8 @@ bar.addEventListener("click", function () {
 cross.addEventListener("click", function () {
   navMenu.classList.remove("active");
 });
+navLink.forEach((n) =>
+  n.addEventListener("click", function () {
+    navMenu.classList.remove("active");
+  })
+);
