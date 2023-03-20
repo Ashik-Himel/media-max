48234f710542756a67e0175dffa54b7619acb59c
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-03-20 22:53:05 +0600

    table data added of 23 employees

--- employees.js
+++ employees.js
@@ -45,12 +45,21 @@ const employeeCardSection = document.querySelector(".employee-card");
 const employeeCardBack = document.querySelector(
   ".employee-card .fa-chevron-left"
 );
-const rowCode = document.querySelectorAll(".row-code");
+const rowID = document.querySelectorAll(".row-id");
 const rowName = document.querySelectorAll(".row-name");
+const rowDesignation = document.querySelectorAll(".row-designation");
 const rowDHName = document.querySelectorAll(".row-dh-name");
 const employeeImg = document.querySelector("#employee-image");
 const employeeName = document.querySelector("#employee-name");
+const employeeDesignation = document.querySelector("#employee-designation");
+const employeeID = document.querySelector("#employee-id");
+const employeePhoneNumber = document.querySelector("#employee-phone-number");
 const employeeDHName = document.querySelector("#employee-dh-name");
+const employeeJobExperience = document.querySelector(
+  "#employee-job-experience"
+);
+const employeeDateOfBirth = document.querySelector("#employee-date-of-birth");
+const employeeBloodGroup = document.querySelector("#employee-blood-group");
 const employeePhone = document.querySelector("#employee-phone");
 
 function cardOpen() {
@@ -60,16 +69,33 @@ function cardOpen() {
   employeeImg.src = this.closest("tr").querySelector(".row-img").src;
   employeeName.innerHTML =
     this.closest("tr").querySelector(".row-name").innerHTML;
-  employeeDHName.innerHTML =
-    "<span style='font-weight: 500; display: inline; font-size: 1.1rem'>Dist. House : </span>" +
+  employeeDesignation.innerHTML =
+    this.closest("tr").querySelector(".row-designation").innerHTML;
+  employeeID.innerHTML += this.closest("tr").querySelector(".row-id").innerHTML;
+  employeePhoneNumber.innerHTML +=
+    this.closest("tr").querySelector(".row-phone").innerHTML;
+  employeeDHName.innerHTML +=
     this.closest("tr").querySelector(".row-dh-name").innerHTML;
+
+  const joinigDate =
+    this.closest("tr").querySelector(".row-joining-date").innerHTML;
+  const expMS = new Date(new Date().getTime() - new Date(joinigDate).getTime());
+  employeeJobExperience.innerHTML += `${
+    expMS.getFullYear() - 1970
+  } Years ${expMS.getMonth()} Months`;
+
+  employeeDateOfBirth.innerHTML +=
+    this.closest("tr").querySelector(".row-date-of-birth").innerHTML;
+  employeeBloodGroup.innerHTML +=
+    this.closest("tr").querySelector(".row-blood-group").innerHTML;
   employeePhone.href = this.closest("tr").querySelector(".row-phone").href;
 }
 function cardClose() {
   employeeCardSection.classList.remove("active");
   tableSection.classList.add("active");
 }
-rowCode.forEach((n) => n.addEventListener("click", cardOpen));
+rowID.forEach((n) => n.addEventListener("click", cardOpen));
 rowName.forEach((n) => n.addEventListener("click", cardOpen));
+rowDesignation.forEach((n) => n.addEventListener("click", cardOpen));
 rowDHName.forEach((n) => n.addEventListener("click", cardOpen));
 employeeCardBack.addEventListener("click", cardClose);
